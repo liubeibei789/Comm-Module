df5a6615f0d1509b976edc926052c5d16c4abeb0
Beibei LIU <liubeibei789@gmail.com>
2018-04-04 09:36:31 -0700

    Add files via upload

--- Comm.py
+++ Comm.py
@@ -11,170 +11,171 @@ import threading
 import time
 import os
 import sys
-import shutil
+
 
 class Comm(object):
 
-    def __init__(self):
+    ############################################
+    # __init__: constructor:
+    # args:
+    # numOfPeers: number of peers to communicate with
+    ############################################
+    def __init__(self, numOfPeers):
 
         self.quitFlag = 0  # if 1: server closed (server)
         self.exitFlag = 0  # if 1: client stops transmitting files (client)
-        self.timeOut = 10     # maximum number of connecting trials (client)
+        self.timeOut = 100     # maximum number of connecting trials (client)
         self.interval = 2     # try re-connecting every 3 second (client)
         self.maxConnect = 5   # maximum number of connections allowed (server)
         self.recvBufSize = 1024  # receiving buffer size
-        self.inService = 1   # if 1: indicating it is during the configuration period
-        self.myIp = '127.0.0.1'
+        self.lock = threading.Lock()  # for the shared parameter self.exitFlag & self.quitFlag, etc.
+
+        self.numOfPeers = numOfPeers  # 1  # number of peers to communicate with
         self.peerIp = '127.0.0.1'
         self.myPort = 1111   # this is the default port, if not working, change it
         self.peerPort = 1111 # this is the default port, if not working, change it
-        self.ini = 0  # if 1: indicating the current machine is the one who initiate the transmission
-        self.fileNameTran = []
-        self.folderNameTran = []
-        self.fileNameRecv = []
-        self.numOfPairs = 1  # number of connection pairs
 
-    ##################################################################
-    # this function runs at the very beginning,
-    # assisting the user to configure this Comm module
-    ##################################################################
-    def config(self):
+        #self.fileList = [[]]
+
+        ####### print the host name and host ip ############
         self.hostName = socket.gethostname()
-        print('Your host name is:' + self.hostName)
+        print('Host name:' + self.hostName)
 
         self.myIp = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [
             [(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in
              [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
 
-        print('Your host ip is: ' + self.myIp)
-        print 'Please let your peer machine know your ip'
-        time.sleep(1)
-        self.numOfPairs = raw_input('How many entities are you going to communicate? Put number and enter:')
-        # self.fileNameRecv = []
-        for i in range(int(self.numOfPairs)):
-            print('for entity_'+ str(i+1) +':')
-            self.peerIp = raw_input('Input your peer machine \'s ip and enter:')
+        print('Host ip: ' + self.myIp)
 
-            folderNameTranTemp = socket.gethostbyaddr(self.peerIp)
-            shutil.move('./fileTran/entity_'+str(i+1), './fileTran/'+ folderNameTranTemp[0])
-            self.folderNameTran.append(folderNameTranTemp[0])
-            fileNameTranTemp = raw_input('Input the shared part of all your transmitting file names (without underscore) and enter:')
-            self.fileNameTran.append(fileNameTranTemp)
-
-        ini_temp = raw_input('Are you the one who initiate the transmission? y/n and enter:')
-        if ini_temp == 'y' or ini_temp == 'Y' or ini_temp == 'yes':
-            self.ini = 1
-        raw_input('Configuration done. Press enter to begin the transmission:')
-
+    ############################################
+    # getList() function:
+    # go through the files for each peer, put file directories to fileList
+    # args:
+    # tranFolderList: a list, each element is a folder directory for a given peer
+    # return:
+    # fileList: a list, each element is a list of files for a given peer
+    ############################################
+    def getList(self, tranFolderList):
+        fileList = []
+        print 'directories of files for all peers are:'
+        for p in range(self.numOfPeers):
+            fileListTemp = []
+            print '*********** file list for peer '+ str(p) + ': ***********'
+            for root, dirs, files in os.walk(tranFolderList[p]):
+                for file in files:
+                    if file == '.DS_Store':  # for the case of mac operating system, to skip .DS_Store file
+                        continue
+                    fullDir = os.path.join(root, file)
+                    print fullDir
+                    fileListTemp.append(fullDir) # fileListTemp: for a given peer (1-dim list)
+            fileList.append(fileListTemp) # for all peers (2-dim, list of list)
+            print 'number of files: '+ str(len(fileListTemp))
+        return fileList
 
     ##################################################################
     # listen to a port for all possible incoming connections
     # write the receiving messages to files, naming with the ip of incoming connection
     # files from different users are stored in separate folders, naming with the ip of incoming connection
     ##################################################################
-    def receive(self):
-        print 'receive() begins'
-        i = 0
+    def receive(self, recvFolderList, peerIpList):
+        print 'receive() begins' # start listening
         sockRecv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sockRecv.bind((self.myIp, self.myPort))
         sockRecv.listen(self.maxConnect)
 
-        dict_customer = {}
+        dict_peer = {}  # dictionary: key: peer host ip, value: structure with 2 members
+                            # member 1: name of receiving folder
+                            # member 2: number of connections from this ip
 
+        ######### create the dictionary ########
+        for i in range(len((peerIpList))):
+            dict_peer[peerIpList[i]] = [recvFolderList[i], 0]
+            if not os.path.exists(recvFolderList[i]):
+                os.mkdir(recvFolderList[i])
+
+        ########### main loop ############
         while self.quitFlag == 0:
-            print 'enter receive()'
-            connection, addr = sockRecv.accept()
-
-            if addr[0] == self.myIp:  # the signal for exit or quit
+            #================== waiting for a new connection ==================
+            print 'enter receive()' 
+            connection, addr = sockRecv.accept()  # blocked here
+            if addr[0] == self.myIp:  # the signal for exit or quit(to break the block)
                 break
-            print 'accepted'
             print self.hostName + ' accepts a connection from:' + str(addr)
             fileNameRecvTemp = socket.gethostbyaddr(addr[0])
             print 'peer name is:', fileNameRecvTemp[0]
-            if fileNameRecvTemp[0] in dict_customer:
-                dict_customer[fileNameRecvTemp[0]] += 1
+
+            # ============== update this new connection in dictionary ==============
+            if addr[0] in dict_peer:
+                recvFolderName = dict_peer[addr[0]][0]  # member 1: receiving folder name
+                dict_peer[addr[0]][1] += 1  # member 2: number of connections from this ip
+
+                buf = connection.recv(self.recvBufSize)  # the first round is file name
+                bufFileName = buf.split('endMarker')[0]   # file = fileName + 'endMarker' + fileContent
+                # if bufFileName == '000':  # closing signal
+                #     print 'closing signal'
+                #     break
+                print 'received file directory is:' + os.path.join(recvFolderName, bufFileName)
+                fd = open(os.path.join(recvFolderName, bufFileName), 'w')
+
+                print '************ content: ************'
+                buf = buf.split('endMarker')[1]
+                while buf:
+                    sys.stdout.softspace = 0  # avoid space between two buffer prints
+                    print buf
+                    fd.write(buf)
+                    buf = connection.recv(self.recvBufSize)
+                print '**********************************'
+                connection.send('feedback: ' + self.hostName + ' has received ' + bufFileName)
+                connection.close()
             else:
-                dict_customer[fileNameRecvTemp[0]] = 1
-                os.mkdir('fileRecv/' + fileNameRecvTemp[0])
-            fd = open('./fileRecv/' + fileNameRecvTemp[0] + '/' + fileNameRecvTemp[0] + '_' + str(
-                    dict_customer[fileNameRecvTemp[0]]) + '.txt', 'w')
-            #self.fileNameRecv = socket.gethostbyaddr(addr[0])
-            #print 'peer name is:', self.fileNameRecv[0]
-            # if self.fileNameRecv[0] in dict_customer:
-            #     dict_customer[self.fileNameRecv[0]] += 1
-            # else:
-            #     dict_customer[self.fileNameRecv[0]] = 1
-            #     os.mkdir('fileRecv/'+ self.fileNameRecv[0])
-            # fd = open('./fileRecv/' + self.fileNameRecv[0] + '/' + self.fileNameRecv[0] + '_' + str(dict_customer[self.fileNameRecv[0]]) + '.txt', 'w')
-            print self.hostName + ' is receiving:'
-            buf = connection.recv(self.recvBufSize)
-            while buf:
-                sys.stdout.softspace = 0  # avoid space between two buffer prints
-                print buf
-                fd.write(buf)
-                buf = connection.recv(self.recvBufSize)
-            connection.send('feedback: ' + self.hostName + ' has received ' + fileNameRecvTemp[0] + '_' + str(dict_customer[fileNameRecvTemp[0]]) + '.txt')
-            self.fileNameRecv.append(fileNameRecvTemp[0])
-            connection.close()
+                print 'unrecognized incoming connection!'
         sockRecv.close()
         print 'receive() done'
 
 
     ##################################################################
-    # check if the last file expected from its peer is received,
-    # if yes, then transmit the current file to its peer
+    # transmit() function:
+    # transmit a bunch of files specified by givenFileList to the given peer
+    # args:
+    # givenFileList: list of files to transmit (for a given peer)
     ##################################################################
-    def transmit(self):
+    def transmit(self, givenFileList, peerIp):
 
-        i = 0
         print 'transmit() begins'
-        while self.exitFlag == 0:
-            for k in range(int(self.numOfPairs)):
-                print '0000000000000', self.fileNameTran[k]
-                print '1111111111111', self.folderNameTran[k]
-                if(i > 0):
-                    print '2222222222222', self.fileNameRecv[k]
-                time.sleep(3)
-                if (i == 0 and self.ini == 1):
-                    print '============ 1 ============'
-                if (i > 0 and os.path.exists('./fileRecv/' + self.fileNameRecv[k] + '/' + self.fileNameRecv[k] + '_' + str(i) + '.txt')):
-                    print '============= 2 =========='
-                    print './fileRecv/' + self.fileNameRecv[k] + '/' + self.fileNameRecv[k] + '_' + str(i) + '.txt'
-                if os.path.isfile('./fileTran/' + self.folderNameTran[k] + '/' + self.fileNameTran[k] + '_' + str(i+1) + '.txt'):
-                    print '============== 3 =========='
-                    print './fileTran/' + self.folderNameTran[k] + '/' + self.fileNameTran[k] + '_' + str(i+1) + '.txt'
+        for k in range(len(givenFileList)):
+            if self.exitFlag == 0:
+                print 'enter transmit()'  # start a new transmission
+                time.sleep(1)
+                fileName = os.path.basename(givenFileList[k])
+                print 'going to send: ' + fileName
+                isConnected = 0       # if 1: host is connected to server
+                j = 1
+                while j <= self.timeOut and isConnected == 0:
+                    print 'trial ', str(j)
+                    sockTran = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # socket creation must be in for loop
+                    try:
+                        sockTran.connect((peerIp, self.peerPort))
+                        print 'connected'
+                        isConnected = 1
+                    except:
+                        print 'connecting...'
+                        time.sleep(self.interval)
+                        j += 1
 
-                if ((i == 0 and self.ini == 1) or (i > 0 and os.path.exists('./fileRecv/' + self.fileNameRecv[k] + '/' + self.fileNameRecv[k] + '_' + str(i) + '.txt'))) \
-                            and os.path.isfile('./fileTran/' + self.folderNameTran[k] + '/' + self.fileNameTran[k] + '_' + str(i+1) + '.txt'):
-                    print 'enter transmit()'
-                    time.sleep(2)
-                    i = i + 1
-                    print 'going to send: ' + './fileTran/' + self.folderNameTran[k] + '/' + self.fileNameTran[k] + '_' + str(i) + '.txt'
-                    isConnected = 0       # if 1: host is connected to server
-                    #for j in range(self.timeOut):
-                    j = 1
-                    while j <= self.timeOut and isConnected == 0:
-                        #if isConnected == 0:
-                        print 'j= ', str(j)
-                        sockTran = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # socket creation must be in for loop
-                        try:
-                            sockTran.connect((self.peerIp, self.peerPort))
-                            print 'connected'
-                            isConnected = 1
-                        except:
-                            print 'connecting...'
-                            time.sleep(self.interval)
-                            #continue
-                            j = j + 1
-                    fd = open('./fileTran/' + self.folderNameTran[k] + '/' + self.fileNameTran[k] + '_' + str(i) + '.txt', 'r')
+                sockTran.send(fileName+'endMarker')
+                fd = open(givenFileList[k], 'r')
+                if fd != -1:
+                    print 'open succeed'
+                line = fd.readline()
+                while line:
+                    sockTran.send(line)
+                    print line,
                     line = fd.readline()
-                    while line:
-                        sockTran.send(line)
-                        print line,
-                        line = fd.readline()
-                    sockTran.close()
+                sockTran.close()
         print 'transmit() done'
-
+        # sockClose = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # sockClose.connect((peerIp, self.peerPort))
+        # sockClose.send('000endMarker')
     ##################################################################
     # when receiving is done, to close server, type 'q' (for quit)
     # when transmitting is done, to stops client, type 'e' (for exit)
@@ -187,15 +188,15 @@ class Comm(object):
                     print 'client stops transmitting'
                     sockExit = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                     sockExit.connect((self.myIp, self.myPort))
-                    lock.acquire()
+                    self.lock.acquire()
                     self.exitFlag = 1
-                    lock.release()
+                    self.lock.release()
                     print self.hostName + ' \'s transmission finished'
                 elif instruction == 'q':
                     print 'server closed'
-                    lock.acquire()
+                    self.lock.acquire()
                     self.quitFlag = 1
-                    lock.release()
+                    self.lock.release()
                     sockQuit = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                     sockQuit.connect((self.myIp, self.myPort))
                     print self.hostName + ' \'s server closed'
@@ -210,25 +211,55 @@ class Comm(object):
 ##################################################################
 # main
 ##################################################################
-if __name__ == '__main__':
+#if __name__ == '__main__':
+    def do(self, numOfPeers, tranFolderList, recvFolderList, peerIpList):
 
-    testComm = Comm()
+        # numOfPeers = 1
+        # testComm = Comm(numOfPeers)
 
-    #shutil.move('./fileTran/beibei-optiplex-390.ecee.dhcp.asu.edu','./fileTran/entity_1')
-    # shutil.move('./fileTran/beibeis-mbp.mobile.asu.edu', './fileTran/entity_2')
+        fileList = []
+        #tranFolderList = [os.path.join('.','fileTran','entity_1'), os.path.join('.','fileTran','entity_2')]
+        fileList = self.getList(tranFolderList)
 
-    testComm.config()
-    threads = []
-    lock = threading.Lock()
-    t1 = threading.Thread(target=testComm.receive)
-    t2 = threading.Thread(target=testComm.transmit)
-    t3 = threading.Thread(target=testComm.terminate)
-    threads.append(t1)
-    threads.append(t2)
-    threads.append(t3)
+        #recvFolderList = [os.path.join('.','fileRecv','entity_1'), os.path.join('.','fileRecv','entity_2')]
+        #peerIpList = ['10.206.203.243', '10.152.144.160']
 
-    for t in threads:
-        t.start()
-    t2.join()
-    t3.join()
+        ###### clear up the receiving folder #########
+        for i in range(len(recvFolderList)):
+            for root, dirs, files in os.walk(recvFolderList[i], topdown=False):
+                for name in files:
+                    os.remove(os.path.join(root, name))
+                for name in dirs:
+                    os.rmdir(os.path.join(root, name))
+
+        ########## multi-threaded ###############
+        threads = []
+
+        #====================== thread 1: receive ======================
+        # listening for the connection
+        t1 = threading.Thread(target=self.receive, args=(recvFolderList, peerIpList))
+        threads.append(t1)
+
+        # ====================== thread 2: terminate ======================
+        # for possible human interrupt during the transmission
+        t2 = threading.Thread(target=self.terminate)
+        threads.append(t2)
+
+        # ====================== thread 3 and more: transmit ======================
+        # for each transmission, a new thread is created
+        for i in range(numOfPeers):  # i: peer index, fileList: two-dimension
+            t3 = threading.Thread(target=self.transmit, args=(fileList[i], peerIpList[i])) # fileList[i]: file list of peer i
+            threads.append(t3)
+
+        ############## start the threads ###########
+        for t in threads:
+            t.start()
+
+        ######### blocked here, wait for termination of all threads ###########
+        for t in threads:
+            t.join()
+
+        # for i in range(numOfPeers):
+        #     threads[i+2].join()
+        # t1.join()
 
